fffef81d99bf0f3c379e4ad690c0284e1b6161a1
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-09-24 09:00:37 -0500

    Create languages_eng.php

---
+++ includes/languages_eng.php
@@ -0,0 +1,13 @@
+<?php
+/**
+  * Pluma
+  * Translations for English (US)
+  * @translators Jeffrey Wang
+*/
+
+$translations = array (
+  'notifications' => 'Notifications',
+  'grades' => 'Grades',
+  'vitals' => 'Vitals',
+  'attendance' => 'Attendance',
+);
