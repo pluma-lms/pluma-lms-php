a1e61c4c26760765eb0fb006c7d781809edcf69b
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-01 09:07:41 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -7,3 +7,11 @@ $organization_settings = array (
   'name' => 'Pluma ISD',
   'image_url' => 'pluma.png',
 );
+
+$database_settings = array (
+  'type' => 'mysql',
+  'host' => 'mysql.mywikis.com',
+  'username' => 'pluma',
+  'password' => 'ha',
+  'database_name' => 'pluma_production',
+);
