cbab4ae94af105472c942937ecf78cb874a9192d
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-15 08:50:49 -0500

    Update user_data.php

--- includes/backend/user_data.php
+++ includes/backend/user_data.php
@@ -32,6 +32,34 @@ class user_data {
       if ( $amount == $i ) {
         break;
       }
+      if ( $iterator->previous() == $iterator->next() ) {
+        $iterator->previous();
+        $iterator->remove();
+      }
+      $ret_text += $iterator->next() + "\n";
+    }
+    return $ret_text;
+  }
+  // in: {userid, lastname, firstname, DOB}
+  function add ( $in_user_arr ) {
+    $database = new database_connect();
+    // pull user data
+    $iterator = new Iterator( $database->userlist );
+    if ( $amount == 0 ) {
+      $amount = sizeof ( $database->userlist );
+    }
+    $i = 0;
+    $ret_text = "";
+    while ( $iterator->hasNext() ) {
+      $i++;
+      if ( $amount == $i ) {
+        $iterator->add( $in_user_arr );
+        break;
+      }
+      if ( $iterator->previous() == $iterator->next() ) {
+        $iterator->previous();
+        $iterator->remove();
+      }
       $ret_text += $iterator->next() + "\n";
     }
     return $ret_text;
