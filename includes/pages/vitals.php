def50684f640c5aa8d508c95b631df63d7429827
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-25 17:20:22 -0500

    Update and rename vitals.php to includes/pages/vitals.php

---
+++ includes/pages/vitals.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+         <meta charset="utf-8"/>
+    </head>
+    <body>
+        <p>Name: Adrian Compton</p>
+        <p>Date of birth: May 14, 2001</p>
+        <p>Gender: Male</p>
+        <p>Race: White</p>
+        <p>Parents/legal guardian: Stray T. Compton (father), Auta Compton (mother)</p>
+        <p>Local student ID: 673043</p>
+        <p>GPA: 4.53</p>
+        <p>Class rank: 1/729</p>
+    </body>
+</html>
