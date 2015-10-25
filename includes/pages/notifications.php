819abcd75c26112b8e39a9b502c765d2ce2a1306
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-25 17:14:26 -0500

    Update and rename notifications.php to includes/pages/notifications.php

---
+++ includes/pages/notifications.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+         <meta charset="utf-8"/>
+    </head>
+    <body>
+        <p><b>Mrs. Takier</b></p>
+        <p>has graded the assignment <b>Exploring Frontiers</b>. You got a <b>100</b> out of <b>100</b>; <b>100%</b>.</p>
+    </body>
+</html>
