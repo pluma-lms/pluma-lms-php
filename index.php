08d982eb216ac87ffd2ffaec2e9019751f1f5959
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-10-25 17:10:38 -0500

    Actually, let's change it to /includes/pages/

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ( empty ( $user_settings['language'] ) ) {
         <title>Pluma</title>
         <script src="./javascript.js"></script>
         <script>
-            var pages=['./index.php?page=notifications', './index.php?page=grades', './index.php?page=vitals','./index.php?page=attendance'];
+            var pages=['./includes/pages/notifications.php', './includes/pages/grades.php', './includes/pages/vitals.php','./includes/pages/attendance.php'];
             var pageIndex=0;
             document.onkeydown = function(e) 
             {
