3eb7091d960dddfe2e2583d24704ed185ffa576c
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-11-02 09:56:22 -0600

    Fixing code formatting

--- includes/backend/user_login.php
+++ includes/backend/user_login.php
@@ -20,17 +20,18 @@ class user_login {
     $this->db = $db_arr[1];
     // continue implementing login check
     $this->username = mysqli_real_escape_string ( $this->db, stripslashes ( $this->username ) );
-		$this->password = mysqli_real_escape_string ( $this->db, stripslashes ( $this->password ) );
-		$getuserquery = "SELECT * FROM user WHERE user_name='" . $this->username . "' AND user_password='" . $this->password . "'";
-		$getuserresult = mysqli_query ( $this->db, $getuserquery );
-		$getuserresult_count = mysqli_num_rows ( $getuserresult );
-		if ( $this->getuserresult_count != 1 ) {
-		  $this->authstatus = false;
-		  return array ( 2, 'Username and password incorrect.' );
-		} else {
-		  $this->authstatus = true;
-		  return array ( 0, $this->authstatus );
-		}
+    $this->password = mysqli_real_escape_string ( $this->db, stripslashes ( $this->password ) );
+    $getuserquery = "SELECT * FROM user WHERE user_name='" . $this->username . "' AND user_password='" . $this->password . "'";
+    $getuserresult = mysqli_query ( $this->db, $getuserquery );
+    $getuserresult_count = mysqli_num_rows ( $getuserresult );
+    if ( $this->getuserresult_count != 1 ) {
+    	$this->authstatus = false;
+    	return array ( 2, 'Username and password incorrect.' );
+    } else {
+    	$this->authstatus = true;
+    	return array ( 0, $this->authstatus );
+    	
+    }
   }
   public function get_authstatus() {
     return $this->authstatus;
