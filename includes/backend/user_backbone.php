b9f21bccb3d087ec6056718dc3f74b152fd4daf3
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-11-02 08:37:26 -0600

    Update user_backbone.php

--- includes/backend/user_backbone.php
+++ includes/backend/user_backbone.php
@@ -7,10 +7,8 @@
 interface user_backbone {
   public function set_username ( $username );
   public function set_password ( $password );
-  public function set_fullname ( $firstname, $middlename, $lastname );
   public function set_group ( $groupname );
   public function get_username ();
   public function get_password ();
-  public function get_fullname ();
   public function get_group ();
 }
