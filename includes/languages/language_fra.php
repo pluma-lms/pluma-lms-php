aa86c1ba6a79817f18fc5268d53d8373c2ae5ed4
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-11-02 18:52:06 -0600

    Update language_fra.php

--- includes/languages/language_fra.php
+++ includes/languages/language_fra.php
@@ -10,4 +10,8 @@ $translations = array (
   'grades' => 'Notes',
   'vitals' => 'Vitals',
   'attendance' => 'Attendance',
+  'pluma' => 'Pluma',
+  'username' => 'Nom d'utilisateur,
+  'password' => 'Mot de passe',
+  'login' => 'Login',
 );
