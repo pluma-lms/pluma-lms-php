e8984abc810c3f56d95c22fe959d5927f564540d
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-11-18 22:42:26 -0600

    Update language_fra.php

--- includes/languages/language_fra.php
+++ includes/languages/language_fra.php
@@ -11,7 +11,12 @@ $translations = array (
   'vitals' => 'Vitals',
   'attendance' => 'Attendance',
   'pluma' => 'Pluma',
-  'username' => 'Nom d'utilisateur,
+  'username' => 'Nom d\'utilisateur',
   'password' => 'Mot de passe',
   'login' => 'Login',
+  'login_success' => 'Login r&eacute;ussi!',
+  'login_success_description' => 'Vous pouvez maintenant entrer Pluma.',
+  'login_failed' => 'Login rat&eacute;',
+  'login_failed_description' => 'Nous ne sommes pas en mesure de vous connecter. Essayer &agrave; nouveau, peut-&ecirc;tre? Ou ici est une raison pour laquelle',
+  'redirecting_notice' => 'Redirection vers 2 secondes, ou cliquez ici pour continuer maintenant.',
 );
